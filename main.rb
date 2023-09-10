fe5c626767b2c2cb40aab12d21c5a22a64f80615
tompng <tomoyapenguin@gmail.com>
2023-09-10 17:58:39 +0900

    minify sixel data

--- main.rb
+++ main.rb
@@ -1,24 +1,9 @@
-=begin
-<ESC>Pq
-#0;2;0;0;0#1;2;100;100;0#2;2;0;100;0
-#1~@v@#2~@~$
-#2?}G}#1?}?-
-#0;2;0;0;0#1;2;50;60;40#2;2;80;30;70
-#1!7@
-<ESC>\
-=end
-
-def sixel
+def sixel(image)
   header = "\ePq"
   footer = "\e\\"
-  color_table = 101.times.map{"##{[_1,2,_1,_1,_1]*';'}"}.join
-  w = 60
-  h = 60
-  image = h.times.map{|i|
-    w.times.map{|j|
-      (Math.sqrt(i**2+j**2)).round % 101
-    }
-  }
+  color_table = 101.times.map { "##{[_1, 2, _1, _1, _1] * ';'}" }.join
+  h = image.size
+  w = image.first.size
   sixel_lines = (h.fdiv(6).ceil).times.map { {} }
   h.times do |y|
     w.times do |x|
@@ -28,22 +13,33 @@ def sixel
   end
   output = [header, color_table]
   sixel_lines.each do |line|
-    (0..100).each do |color|
-      bitslist = line[color]
-      next unless bitslist
-      output << "###{color}"
-      w.times do |x|
-        output << (63+bitslist[x]).chr
+    linedata = []
+    line.each do |color, bitmasks|
+      linedata << '$' unless linedata.empty?
+      linedata << "##{color}"
+      prev_x = -1
+      bitmasks.sort_by{_1}.each do |x, bitmask|
+        linedata << (prev_x + 2 == x ? '?' : "!#{x - prev_x - 1}?") if prev_x + 1 < x
+        linedata << (63 + bitmask).chr
+        prev_x = x
       end
-      output << '$'
     end
-    output << '-'
+    output << linedata << '-'
   end
-  puts [output, footer].join
-  data = 10.times.map{
-    "###{rand 101}#{60.times.map{(63+rand(64)).chr}}-"
-  }
-  puts [header,color_table,data, footer].join
+  [output, footer].join
 end
 
-sixel
+$> << "\e[H\e[2J"
+(0..).each do |t|
+  w = h = 240
+  image = h.times.map{|i|
+    w.times.map{|j|
+      # Calculate color(grayscale, 0..100) for each pixel
+      (Math.sqrt(i**2+j**2)-t+Math.sin(0.1*(i-4*t))+Math.sin(0.1*(j-4*t))).round % 101
+    }
+  }
+
+  sixel_data = sixel(image)
+  $> << "\e[H"+sixel_data+"\r\n\e[Ksixel_bytes = #{sixel_data.size}\n\e[K"
+  sleep 0.01
+end
