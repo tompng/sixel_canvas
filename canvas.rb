bab86cdc697a15249bcf6bda76554ca922f3702c
tompng <tomoyapenguin@gmail.com>
2023-09-15 03:35:18 +0900

    varying linewidth

--- canvas.rb
+++ canvas.rb
@@ -2,58 +2,72 @@ require_relative 'sixel'
 
 class Path
   attr_reader :dot_points, :paths
-  def initialize(canvas, &block)
+  def initialize(canvas, line_width)
     @current_path = []
     @dot_points = []
     @paths = [@current_path]
     @canvas = canvas
     @x = @y = nil
-    instance_exec(&block) if block
+    @line_width = line_width.to_i
+    yield self if block_given?
   end
 
-  def dot(x, y)
-    @dot_points << [x, y]
+  def dot(x, y, radius = @line_width / 2.0)
+    @dot_points << [x, y, 2.0 * radius]
     self
   end
 
-  def dots(points)
-    @dot_points.concat points
+  def dots(points, radius = @line_width / 2.0)
+    @dot_points.concat points.map { |x, y, r| [x, y, (r || radius) * 2.0] }
     self
   end
 
-  def polygon(points)
+  def polygon(points, line_width = @line_width)
     return self if points.empty?
 
-    prev = points.first
+    x0, y0, w0 = points.first
+    prev = [x0, y0, w0 || line_width]
     move_to(*prev)
     (1...points.size).each do |i|
-      p = points[i]
+      x, y, w = points[i]
+      p = [x, y, w || line_width]
       @canvas._line_path prev, p, @current_path
       prev = p
     end
-    @x, @y = prev
+    @x, @y, @line_width = prev
     self
   end
 
-  def line_to(x, y)
+  def line_to(x, y, line_width = @line_width)
     return self unless @x && @y
 
-    @canvas._line_path [@x, @y], [x, y], @current_path
+    @canvas._line_path [@x, @y, @line_width], [x, y, line_width], @current_path
     @x = x
     @y = y
+    @line_width = line_width
     self
   end
 
-  def bezier_curve_to(x1, y1, x2, y2, x3, y3)
+  def bezier_curve_to((x1, y1, w1), (x2, y2, w2), (x3, y3, w3), line_width = @line_width)
     return self unless @x && @y
 
-    @canvas._bezier_path [@x, @y], [x1, y1], [x2, y2], [x3, y3], @current_path
+    @canvas._bezier_path [@x, @y, @line_width], [x1, y1, w1 || line_width], [x2, y2, w2 || line_width], [x3, y3, w3 || line_width], @current_path
     @x = x3
     @y = y3
+    @line_width = w3 || line_width
     self
   end
 
-  def move_to(x, y)
+  def stroke(line_width: nil, alpha: nil, color: nil)
+    @canvas.stroke(self, **{ line_width:, alpha:, color: }.compact)
+  end
+
+  def fill(alpha: nil, color: nil)
+    @canvas.fill(self, **{ alpha:, color: }.compact)
+  end
+
+  def move_to(x, y, line_width = @line_width)
+    @line_width = line_width
     return self if @x == x && @y == y
 
     @x = x
@@ -76,18 +90,11 @@ class Canvas
   TERMINAL_CLEAR_SCREEN = "\e[H\e[2J"
   TERMINAL_CURSOR_RESET = "\e[H"
 
-  attr_reader :width, :height, :pixels, :color, :line_width
+  attr_reader :width, :height, :pixels, :color
   def initialize(width, height, colors: nil, antialias: 3)
     @width = width
     @height = height
     @colors = colors
-    if @colors
-      self.color = 1.0
-    else
-      self.color = 0xffffff
-    end
-    @alpha = 1.0
-    @line_width = 1.0
     @antialias = antialias.to_i.clamp 1, 8
     @antialias_area = @antialias**2
     @pixels = @height.times.map do
@@ -95,8 +102,8 @@ class Canvas
     end
   end
 
-  def new_path(&block)
-    Path.new self, &block
+  def new_path(line_width: 1, &block)
+    Path.new self, line_width, &block
   end
 
   def clear(color = 0, alpha: 1)
@@ -127,36 +134,27 @@ class Canvas
     end
   end
 
-  def color=(color)
+  def default_color
+    @colors ? 1 : 0xffffff
+  end
+
+  def deconstruct_color(color)
     if @colors
-      @color = color.clamp(0, 1).to_f
+      [0, 0, 0, color.clamp(0, 1).to_f]
     else
-      @color = color.to_i
-      @r = (@color >> 16) & 0xff
-      @g = (@color >> 8) & 0xff
-      @b = @color & 0xff
+      c = color.to_i
+      r = (c >> 16) & 0xff
+      g = (c >> 8) & 0xff
+      b = c & 0xff
+      [r, g, b, 0]
     end
   end
 
-  def alpha=(alpha)
-    @alpha = alpha.clamp(0, 1).to_f
-  end
-
-  def line_width=(line_width)
-    @line_width = [1, line_width].max
-  end
-
-  # def bezier(a, b, c, d)
-  #   points = []
-  #   _bezier_path a, b, c, d, points
-  #   _stroke points, @line_width * @antialias / 2
-  # end
-
   def _bezier_path(pa, pb, pc, pd, points)
-    ax, ay = pa.map { _1 * @antialias }
-    bx, by = pb.map { _1 * @antialias }
-    cx, cy = pc.map { _1 * @antialias }
-    dx, dy = pd.map { _1 * @antialias }
+    ax, ay, aw = pa.map { _1 * @antialias }
+    bx, by, bw = pb.map { _1 * @antialias }
+    cx, cy, cw = pc.map { _1 * @antialias }
+    dx, dy, dw = pd.map { _1 * @antialias }
     dab = [(bx - ax).abs, (by - ay).abs].max
     dcd = [(dx - cx).abs, (dy - cy).abs].max
     dad = [(dx - ax).abs, (dy - ay).abs].max
@@ -169,28 +167,38 @@ class Canvas
     y1 = 3 * (by - ay)
     y2 = 3 * (ay - 2 * by + cy)
     y3 = -ay + 3 * by - 3 * cy + dy
-
+    w0 = aw
+    w1 = 3 * (bw - aw)
+    w2 = 3 * (aw - 2 * bw + cw)
+    w3 = -aw + 3 * bw - 3 * cw + dw
     (0..step).each do |i|
       t = i.fdiv step
       x = x0 + x1 * t + x2 * t**2 + x3 * t**3
       y = y0 + y1 * t + y2 * t**2 + y3 * t**3
-      points << [x.round, y.round]
+      w = [w0 + w1 * t + w2 * t**2 + w3 * t**3, 1].max
+      points << [x.round, y.round, w]
     end
   end
 
-  def draw_line(p1, p2)
-    stroke new_path.move_to(*p1).line_to(*p2)
+  def stroke_line(p1, p2, line_width: 1, alpha: 1, color: default_color)
+    stroke(new_path(line_width:).move_to(*p1).line_to(*p2), alpha:, color:)
   end
 
+  def stroke_bezier(a, b, c, d, line_width: 1, alpha: 1, color: default_color)
+    stroke(new_path(line_width:).move_to(*a).bezier_curve_to(b, c, d), alpha:, color:)
+  end
+
+
   def _line_path(p1, p2, points)
-    x1, y1 = p1.map { _1 * @antialias }
-    x2, y2 = p2.map { _1 * @antialias }
+    x1, y1, w1 = p1.map { _1 * @antialias }
+    x2, y2, w2 = p2.map { _1 * @antialias }
     step = [(x2 - x1).abs, (y2 - y1).abs, 1].max
     (0..step).each do |i|
       t = i.fdiv step
       x = (x1 + (x2 - x1) * t).round
       y = (y1 + (y2 - y1) * t).round
-      points << [x, y]
+      w = [w1 + (w2 - w1) * t, 1].max
+      points << [x, y, w]
     end
   end
 
@@ -261,7 +269,9 @@ class Canvas
     ranges
   end
 
-  def fill(path)
+  def fill(path, alpha: 1, color: default_color)
+    alpha = alpha.clamp(0, 1).to_f
+    color = deconstruct_color color
     row_xor_bounds_a = {}
     path.paths.each do |points|
       dedup_points = []
@@ -299,11 +309,12 @@ class Canvas
     end
     row_bounds.each do |pixel_y, antialias_x_bounds|
       next if pixel_y < 0 || pixel_y >= @height
-      _fill_antialias_bounds(pixel_y, antialias_x_bounds)
+      _fill_antialias_bounds(pixel_y, antialias_x_bounds, color, alpha)
     end
   end
 
-  def _fill_antialias_bounds(pixel_y, antialias_x_bounds)
+  def _fill_antialias_bounds(pixel_y, antialias_x_bounds, color, alpha)
+    cr, cg, cb, cvalue = color
     pixels_row = @pixels[pixel_y]
     subtract_bounds = []
     updates = Hash.new 0
@@ -339,61 +350,48 @@ class Canvas
     end
     if @colors
       updates.each do |x, count|
-        alpha = @alpha * count / @antialias_area
-        pixels_row[x] = pixels_row[x] * (1 - alpha) + @color * alpha
+        a = alpha * count / @antialias_area
+        pixels_row[x] = pixels_row[x] * (1 - a) + cvalue * a
       end
     else
       updates.each do |x, count|
-        alpha = @alpha * count / @antialias_area
+        a = alpha * count / @antialias_area
         col = pixels_row[x]
         r = (col >> 16) & 0xff
         g = (col >> 8) & 0xff
         b = col & 0xff
-        r = r * (1 - alpha) + @r * alpha
-        g = g * (1 - alpha) + @g * alpha
-        b = b * (1 - alpha) + @b * alpha
+        r = r * (1 - a) + cr * a
+        g = g * (1 - a) + cg * a
+        b = b * (1 - a) + cb * a
         pixels_row[x] = (r.round << 16) | (g.round << 8) | b.round
       end
     end
   end
 
-  def stroke(path)
-    x_by_y = {}
-    y_bounds = []
-    radius = @line_width * @antialias / 2.0
-    radius_i = radius.ceil
+  def stroke(path, line_width: nil, alpha: 1, color: default_color)
+    alpha = alpha.clamp(0, 1).to_f
+    color = deconstruct_color color
+    row_bounds_a = {}
+
     add_bounds = -> points do
-      points.each do |x, y|
-        (x_by_y[y] ||= {})[x] = true
-        y_bounds << y - radius_i
-        y_bounds << y + radius_i + 0.5
+      points.each do |x, y, w|
+        radius = (line_width || w) / 2.0
+        ((y - radius).ceil..(y + radius)).each do |target_y|
+          pixel_y = target_y / @antialias
+          next if pixel_y < 0 || pixel_y >= @height
+          x_bounds = (row_bounds_a[pixel_y] ||= [])[target_y % @antialias] ||= []
+          dx2 = radius**2 - (target_y - y)**2
+          next unless dx2 > 0
+          dx = Math.sqrt(dx2).floor
+          x_bounds << x - dx << x + dx + 0.5
+        end
       end
     end
     add_bounds.call path.dot_points
     path.paths.each(&add_bounds)
 
-    pixel_y_set = {}
-    each_merged_range_value y_bounds do |target_y|
-      y = target_y / @antialias
-      pixel_y_set[y] = true if 0 <= y && y < @height
-    end
-    pixel_y_set.each_key do |pixel_y|
-      antialias_x_bounds = @antialias.times.map do |i|
-        target_y = pixel_y * @antialias + i
-        x_bounds = []
-        (-radius_i..radius_i).each do |dy|
-          xs = x_by_y[target_y + dy]
-          next unless xs
-
-          dx2 = (radius + 0.5)**2 - dy**2
-          next unless dx2 > 0
-
-          dx = Math.sqrt(dx2).floor
-          xs.each_key do |x|
-            x_bounds << x - dx
-            x_bounds << x + dx + 0.5
-          end
-        end
+    row_bounds_a.each do |pixel_y, antialias_x_bounds|
+      compact_antialias_x_bounds = antialias_x_bounds.compact.map do |x_bounds|
         compact_x_bounds = []
         each_overlapped_range x_bounds do |from, to|
           if compact_x_bounds.last&.> from
@@ -405,7 +403,30 @@ class Canvas
         end
         compact_x_bounds
       end
-      _fill_antialias_bounds(pixel_y, antialias_x_bounds)
+      _fill_antialias_bounds(pixel_y, compact_antialias_x_bounds, color, alpha)
+    end
+  end
+
+  def self.colors(type)
+    themes = {
+      grayscale: -> { (0..100).map { (_1 * 255 / 100) * 0x10101 } },
+      red: [1, 2, 2],
+      green: [2, 1, 2],
+      blue: [2, 2, 1],
+      cyan: [2, 1, 1],
+      magenta: [1, 2, 1],
+      yellow: [1, 1, 2]
+    }
+    theme = themes[type]
+    raise ArgumentError, "Unknown theme #{type}. Should be one of #{themes.keys}" unless theme
+    return theme.call if theme.is_a? Proc
+    r_ex, g_ex, b_ex = theme
+    colors = 256.times.map do
+      t = _1.fdiv 255
+      r = ((t ** r_ex) * 255).round
+      g = ((t ** g_ex) * 255).round
+      b = ((t ** b_ex) * 255).round
+      (r << 16) | (g << 8) | b
     end
   end
 
--- main.rb
+++ main.rb
@@ -3,39 +3,27 @@ require_relative 'canvas'
 use_rgb = rand < 0.5
 # use_rgb: color = 0xRRGGBB
 # indexed: color = 0.0..1.0 with color table
-unless use_rgb
-  colors = 256.times.map do |i|
-    t = i.fdiv 255
-    r, g, b = [t**3, t**2, t].map { (_1 * 255).round }
-    (r << 16) | (g << 8) | b
-  end
-end
-canvas = Canvas.new(240, 240, colors:)
+
+canvas = Canvas.new(256, 256, colors: use_rgb ? nil : Canvas.colors(:blue))
 
 puts Canvas::TERMINAL_CLEAR_SCREEN
 loop do
-  canvas.clear 0, alpha: 0.1
-  canvas.alpha = 0.5
-  canvas.color = use_rgb ? rand(0xffffff) : 1.0
-  canvas.line_width = 16
-  canvas.draw_line [rand(200), rand(200)], [rand(200), rand(200)]
-  canvas.line_width = 2
-  canvas.color = canvas.color = use_rgb ? rand(0xffffff) : 0.8
-  path = canvas.new_path do
-    move_to rand(200), rand(200)
-    line_to rand(200), rand(200)
-    line_to rand(200), rand(200)
-    line_to rand(200), rand(200)
+  canvas.clear 0, alpha: 0.2
+  color1 = use_rgb ? rand(0xffffff) : 1.0
+  color2 = use_rgb ? rand(0xffffff) : 0.8
+
+  canvas.stroke_line [rand(256), rand(256)], [rand(256), rand(256)], line_width: 16, color: color1, alpha: 0.1
+  canvas.new_path line_width: 4 do |path|
+    path.move_to rand(256), rand(256), 10
+    path.bezier_curve_to [rand(256), rand(256), 7], [rand(256), rand(256), 4], [rand(256), rand(256), 1]
+    path.stroke(alpha: 1, color: color2)
   end
-  canvas.alpha = 0.8
-  canvas.stroke(path)
 
   path = canvas.new_path
-  path.move_to rand(200), rand(200)
-  4.times {
-    path.bezier_curve_to(*6.times.map { rand(180) })
-  }
-  canvas.alpha = 0.4
-  canvas.fill(path)
+  path.move_to rand(256), rand(256)
+  4.times do
+    path.bezier_curve_to(*3.times.map { [rand(256), rand(256)] })
+  end
+  canvas.fill(path, alpha: 0.1, color: color2)
   puts Canvas::TERMINAL_CURSOR_RESET + canvas.to_sixel
 end
